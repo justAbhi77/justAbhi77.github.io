38a7bdf3479c6fb2cfd9c312633aea0c0b9ffd89
justAbhi77 <ojhaabhinav2016@gmail.com>
2023-07-09 17:58:13 +0530

    Added a no caching policy and updated projects.

--- Portfolio/About/js/main.js
+++ Portfolio/About/js/main.js
@@ -1,40 +1,46 @@
 // Disable preloader after page is loaded
 preLoader = document.getElementById("preloader");
-window.addEventListener('load', (e) => {
-    preLoader.hidden = true;
+window.addEventListener("load", (e) => {
+  preLoader.hidden = true;
+  populateProjectsImage();
 });
 
 // Add projects to webpage when page is ready
-$(document).ready(function () {
-    const gridDiv = $("#grid");
-    $.getJSON("json/projects.json", function (jsonData) {
+$(document).ready(() => {
+  populateProjects();
+});
 
-        for (let i = 0; i < jsonData.length; ++i) {
+var projectImages = [];
 
-            var linksText = ``;
-            if (jsonData[i].links.length > 0) {
-                for (let j = 0; j < jsonData[i].links.length; ++j) {
-                    linksText += `<a href="${jsonData[i].links[j].url}" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-${jsonData[i].links[j].id} fa-lg"></i></a>`;
-                }
-            }
+function populateProjects() {
+  const gridDiv = $("#grid");
 
-            var tagsText = ``;
-            if (jsonData[i].tags.length > 0) {
-                for (let j = 0; j < jsonData[i].tags.length; ++j) {
-                    tagsText += `<span class="tag-card">${jsonData[i].tags[j]}</span>`;
-                }
-            }
+  $.getJSON("json/projects.json", function (jsonData) {
+    for (let i = 0; i < jsonData.length; ++i) {
+      var linksText = ``;
+      if (jsonData[i].links.length > 0) {
+        for (let j = 0; j < jsonData[i].links.length; ++j) {
+          linksText += `<a href="${jsonData[i].links[j].url}" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-${jsonData[i].links[j].id} fa-lg"></i></a>`;
+        }
+      }
 
-            var descriptionText = ``;
-            if (Boolean(jsonData[i].description)) {
-                descriptionText = jsonData[i].description;
-            }
+      var tagsText = ``;
+      if (jsonData[i].tags.length > 0) {
+        for (let j = 0; j < jsonData[i].tags.length; ++j) {
+          tagsText += `<span class="tag-card">${jsonData[i].tags[j]}</span>`;
+        }
+      }
 
-            var projectElement = `<div class="box">
+      var descriptionText = ``;
+      if (Boolean(jsonData[i].description)) {
+        descriptionText = jsonData[i].description;
+      }
+
+      var projectElement = `<div class="box">
                                     <div class="box-content">
                                         <article>
                                             <div class="img-cont">
-                                            <img src="${jsonData[i].feature_image}" alt="${jsonData[i].feature_image_alt}">
+                                            <img src="" alt="${jsonData[i].feature_image_alt}">
                                             </div>
                                             <h4>${jsonData[i].name}</h4>
                                             <p class="project-rel-links">
@@ -50,22 +56,40 @@ $(document).ready(function () {
                                     </div>
                                 </div>`;
 
-            gridDiv.append(projectElement);
-        }
-    });
-}); 
+      gridDiv.append(projectElement);
+      projectImages.push(jsonData[i].feature_image);
+    }
+  });
+}
 
 // TagCloud
 const Texts = [
-    'CPP', 'C', 'Java', 'JavaFx', 'Python', 'C#', 'Unreal', 'Unity', 'Godot', 'QT', 'MySQL', 'OpenGL', 'Blender', 'Photoshop', 'Premiere Pro', 'HTML', 'CSS', 'JS'
+  "C",
+  "CPP",
+  "Java",
+  "JavaFx",
+  "Python",
+  "C#",
+  "Unreal",
+  "Unity",
+  "Godot",
+  "QT",
+  "MySQL",
+  "OpenGL",
+  "Blender",
+  "Photoshop",
+  "Premiere Pro",
+  "HTML",
+  "CSS",
+  "JS",
 ];
 
-var tagCloud = TagCloud('.Sphere', Texts, {
-    radius: 230,
-    maxSpeed: 'normal',
-    initSpeed: 'fast',
-    direction: 135,
-    keep: true
+var tagCloud = TagCloud(".Sphere", Texts, {
+  radius: 230,
+  maxSpeed: "normal",
+  initSpeed: "fast",
+  direction: 135,
+  keep: true,
 });
 
 // Website Navigation
@@ -82,28 +106,41 @@ const skillsBtn = document.getElementById("skills-btn");
 const acadBtn = document.getElementById("acad-btn");
 
 // Prevent Right Click
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
 });
 
-aboutBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    aboutSec.hidden = false;
-    skillsSec.hidden = false;
-    acadSec.hidden = true;
-    resumeBtn.hidden = false;
+aboutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutSec.hidden = false;
+  skillsSec.hidden = false;
+  acadSec.hidden = true;
+  resumeBtn.hidden = false;
 
-    mainTitle.innerText = "Hi, I am Abhinav Ojha.";
-    titleTag.innerText = "An Aspiring Game Developer.";
+  mainTitle.innerText = "Hi, I am Abhinav Ojha.";
+  titleTag.innerText = "An Aspiring Game Developer.";
 });
 
-acadBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    aboutSec.hidden = true;
-    skillsSec.hidden = true;
-    acadSec.hidden = false;
-    resumeBtn.hidden = true;
+acadBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutSec.hidden = true;
+  skillsSec.hidden = true;
+  acadSec.hidden = false;
+  resumeBtn.hidden = true;
 
-    mainTitle.innerText = "Academics.";
-    titleTag.innerText = "A Gift that none can take away.";
+  mainTitle.innerText = "Academics.";
+  titleTag.innerText = "A Gift that none can take away.";
 });
+
+function populateProjectsImage() {
+  var gridDiv = document.getElementById("grid");
+
+  var gridChildren = gridDiv.children;
+
+  for (let index = 0; index < gridChildren.length; index++) {
+
+    var imgcontainer = gridChildren[index].children[0].children[0].children[0].children[0];
+
+    imgcontainer.src = projectImages[index];
+  }
+}
